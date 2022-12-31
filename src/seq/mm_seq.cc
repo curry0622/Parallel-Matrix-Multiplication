de2d4be1aeaa6ca6e6c37bd073170bf53413a91f
curry0622 <kingkazma0112305@gmail.com>
2022-12-31 14:15:17 +0800

    feat: mm seq version

--- src/seq/mm_seq.cc
+++ src/seq/mm_seq.cc
@@ -1,6 +1,42 @@
 #include <stdio.h>
+#include <assert.h>
 
-int main() {
-    printf("Sequential version of matrix multiplication\n");
+int main(int argc, char *argv[]) {
+    // Read inputs
+    assert(argc == 3);
+    FILE *f = fopen(argv[1], "r");
+    assert(f);
+    int m, n, l;
+    fscanf(f, "%d %d %d", &m, &n, &l);
+    int *a = new int[m * n];
+    int *b = new int[n * l];
+    int *c = new int[m * l];
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            fscanf(f, "%d", &a[i * n + j]);
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < l; j++)
+            fscanf(f, "%d", &b[i * l + j]);
+    fclose(f);
+
+    // Multiply a and b
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < l; j++) {
+            c[i * l + j] = 0;
+            for (int k = 0; k < n; k++)
+                c[i * l + j] += a[i * n + k] * b[k * l + j];
+        }
+    }
+    
+    // Output c to file
+    f = fopen(argv[2], "w");
+    assert(f);
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < l; j++)
+            fprintf(f, "%d ", c[i * l + j]);
+        fprintf(f, "\n");
+    }
+    fclose(f);
+
     return 0;
 }
