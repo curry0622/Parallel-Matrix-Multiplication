40be26ead1179db51a0e780f570c5ce4c379677e
curry0622 <kingkazma0112305@gmail.com>
2023-01-06 17:29:28 +0800

    feat: basic mul code with bugs

--- src/1gpu/mm_1gpu.cu
+++ src/1gpu/mm_1gpu.cu
@@ -1,6 +1,74 @@
 #include <stdio.h>
 #include <assert.h>
-#include <time.h>
+
+#define B 32
+
+int *a, *b, *c;
+int m, n, l;
+
+void input(char *filename) {
+    // Open file
+    FILE *f = fopen(filename, "r");
+    assert(f);
+
+    // Read m, n, l
+    fscanf(f, "%d %d %d", &m, &n, &l);
+
+    // Allocate memory
+    cudaMallocHost((void**)&a, m * n * sizeof(int));
+    cudaMallocHost((void**)&b, n * l * sizeof(int));
+    cudaMallocHost((void**)&c, m * l * sizeof(int));
+
+    // Read a
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            fscanf(f, "%d", &a[i * n + j]);
+
+    // Read b
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < l; j++)
+            fscanf(f, "%d", &b[i * l + j]);
+
+    // Close file
+    fclose(f);
+}
+
+void output(char *filename) {
+    // Open file
+    FILE *f = fopen(filename, "w");
+    assert(f);
+
+    // Write c
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < l; j++)
+            fprintf(f, "%ld ", c[i * l + j]);
+        fprintf(f, "\n");
+    }
+
+    // Close file
+    fclose(f);
+}
+
+__global__ void multiply_gpu(int *d_a, int *d_b, int *d_c, int m, int n, int l) {
+    int row_idx = blockIdx.y * blockDim.y + threadIdx.y;
+    int col_idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if(row_idx >= m || col_idx >= l) return;
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+        sum += d_a[row_idx * n + i] * d_b[i * l + col_idx];
+    d_c[row_idx * l + col_idx] = sum;
+}
+
+void multiply_naive(int *a, int *b, int *c, int m, int n, int l) {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < l; j++) {
+            int sum = 0;
+            for (int k = 0; k < n; k++)
+                sum += a[i * n + k] * b[k * l + j];
+            c[i * l + j] = sum;
+        }
+    }
+}
 
 void multiply_cache_friendly(int *a, int *b, int *c, int m, int n, int l) {
     for (int i = 0; i < m; i++) {
@@ -13,47 +81,28 @@ void multiply_cache_friendly(int *a, int *b, int *c, int m, int n, int l) {
 }
 
 int main(int argc, char *argv[]) {
-    // Timer start
-    clock_t prog_t, cpu_t;
-    prog_t = clock();
+    // Argument check
+    assert(argc == 3);
 
     // Read inputs
-    assert(argc == 3);
-    FILE *f = fopen(argv[1], "r");
-    assert(f);
-    int m, n, l;
-    fscanf(f, "%d %d %d", &m, &n, &l);
-    int *a = new int[m * n];
-    int *b = new int[n * l];
-    int *c = new int[m * l];
-    for (int i = 0; i < m; i++)
-        for (int j = 0; j < n; j++)
-            fscanf(f, "%d", &a[i * n + j]);
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < l; j++)
-            fscanf(f, "%d", &b[i * l + j]);
-    fclose(f);
+    input(argv[1]);
 
-    // multiply a and b
-    cpu_t = clock();
-    multiply_cache_friendly(a, b, c, m, n, l);
-    cpu_t = clock() - cpu_t;
+    // Allocate memory on device
+    int *d_a, *d_b, *d_c;
+    cudaMalloc((void**)&d_a, m * n * sizeof(int));
+    cudaMalloc((void**)&d_b, n * l * sizeof(int));
+    cudaMalloc((void**)&d_c, m * l * sizeof(int));
+
+    // Multiply a and b
+    printf("Multiplying a and b on GPU...");
+    multiply_gpu<<<dim3(l / B, m / B), dim3(B, B)>>>(d_a, d_b, d_c, m, n, l);
+    printf("done\n");
+
+    // Copy result from device to host
+    cudaMemcpy(c, d_c, m * l * sizeof(int), cudaMemcpyDeviceToHost);
     
     // Output c to file
-    f = fopen(argv[2], "w");
-    assert(f);
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < l; j++)
-            fprintf(f, "%ld ", c[i * l + j]);
-        fprintf(f, "\n");
-    }
-    fclose(f);
-
-    // Timer end
-    prog_t = clock() - prog_t;
-    printf("Total time: %f seconds\n", (double)prog_t / CLOCKS_PER_SEC);
-    printf("CPU time: %f seconds\n", (double)cpu_t / CLOCKS_PER_SEC);
-    printf("IO time: %f seconds\n", (double)(prog_t - cpu_t) / CLOCKS_PER_SEC);
+    output(argv[2]);
 
     return 0;
 }
