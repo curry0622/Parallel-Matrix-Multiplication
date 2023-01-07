dd728f0065c341fa2cc6684c0753793db5f978fd
vickychen928 <vicky022038@gmail.com>
2023-01-07 20:40:45 +0800

    feat: read input with fread and mmap-io (have bug)

--- src/seq/mm_seq.cc
+++ src/seq/mm_seq.cc
@@ -5,6 +5,126 @@
 #include <smmintrin.h>
 #include <pmmintrin.h>
 
+#include <cstring>
+
+// 用lseek要include這兩個
+#include <sys/types.h>
+#include <unistd.h>
+// 用mmapio要include這兩個
+#include <sys/mman.h>
+#include <fcntl.h>
+
+int m, n, l;
+int *a, *b, *c;
+
+void input_original(char *filename){
+    FILE *f = fopen(filename, "r");
+    assert(f);
+    fscanf(f, "%d %d %d", &m, &n, &l);
+    a = new int[m * n];
+    b = new int[n * l];
+    c = new int[m * l];
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            fscanf(f, "%d", &a[i * n + j]);
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < l; j++)
+            fscanf(f, "%d", &b[i * l + j]);
+    fclose(f);
+}
+
+void input_v2(char *filename){     // can't use
+    FILE *f = fopen(filename, "r");
+    assert(f);
+    int fd = open(filename, O_RDONLY);
+    off_t totoal_num_of_bytes = lseek(fd, 0, SEEK_END);
+    printf("File size is %ld bytes.\n", totoal_num_of_bytes);
+
+    int datas[totoal_num_of_bytes+1];
+    char readDatas[totoal_num_of_bytes+1];
+    char seps[] = " ";
+    char *token = NULL;
+    int i = 0;
+
+    memset(readDatas, 0x0, sizeof(readDatas));
+    fread(readDatas, sizeof(char), totoal_num_of_bytes, f);
+    // readDatas[totoal_num_of_bytes-1] = 0x0;   // 不知道這行有什麼作用
+    fclose(f);
+
+    token = strtok(readDatas, seps);
+    while(token != NULL){
+        datas[i] = atoi(token);
+        i++;
+        token = strtok(NULL, seps);
+    }
+
+    m = datas[0]; n = datas[1]; l = datas[2];
+    a = new int[m * n];
+    b = new int[n * l];
+    c = new int[m * l];
+    for (int i=0; i<m; i++)
+        for (int j=0; j<n; j++)
+            a[i * n + j] = datas[i * n + j + 3];
+    for (int i=0; i<n; i++)
+        for (int j = 0; j < l; j++)
+            b[i * l + j] = datas[i * l + j + m * n + 3];
+}
+
+void input_v3(char *filename){
+    int fd = open(filename, O_RDONLY);
+    off_t sz = lseek(fd, 0, SEEK_END);
+    printf("File size is %ld bytes.\n", sz);
+
+    char *buf = (char*) mmap(NULL, sz, PROT_READ, MAP_SHARED, fd, 0);
+    close(fd);
+    char *current_char = buf;
+    long long current_num = 0;
+
+    int datas[sz];
+    int count = 0;
+    bool saved = false;
+    while (*current_char != '\0'){ // 字符串結尾標示'\0'
+        if (*current_char != ' ' && *current_char != '\n'){
+            current_num = 10 * current_num + *current_char - '0';  // 每次取出數字第一位
+            saved = false;
+        }
+        else if (saved == false){ 
+            // printf("current_num = %d\n", current_num);
+            datas[count] = current_num;
+            current_num = 0;
+            count++;
+            saved = true;
+        }
+        current_char++;
+    }
+
+    m = datas[0]; n = datas[1]; l = datas[2];
+    a = new int[m * n];
+    b = new int[n * l];
+    c = new int[m * l];
+    for (int i=0; i<m; i++)
+        for (int j=0; j<n; j++)
+            a[i * n + j] = datas[i * n + j + 3];
+    for (int i=0; i<n; i++)
+        for (int j = 0; j < l; j++)
+            b[i * l + j] = datas[i * l + j + m * n + 3];
+
+    // // print matrixs
+    // printf("m = %d, n = %d, l = %d\n", m, n, l);
+    // printf("matrix a: \n");
+    // for (int i=0; i<m; i++){
+    //     for (int j=0; j<n; j++)
+    //         printf("%d ", a[i * n + j]);
+    //     printf("\n");
+    // }
+    // printf("matrix b: \n");
+    // for (int i=0; i<n; i++){
+    //     for (int j = 0; j < l; j++)
+    //         printf("%d ", b[i * l + j]);
+    //     printf("\n");
+    // }
+}
+
 void multiply_naive(int *a, int *b, int *c, int m, int n, int l) {
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < l; j++) {
@@ -89,20 +209,7 @@ int main(int argc, char *argv[]) {
 
     // Read inputs
     assert(argc == 3);
-    FILE *f = fopen(argv[1], "r");
-    assert(f);
-    int m, n, l;
-    fscanf(f, "%d %d %d", &m, &n, &l);
-    int *a = new int[m * n];
-    int *b = new int[n * l];
-    int *c = new int[m * l];
-    for (int i = 0; i < m; i++)
-        for (int j = 0; j < n; j++)
-            fscanf(f, "%d", &a[i * n + j]);
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < l; j++)
-            fscanf(f, "%d", &b[i * l + j]);
-    fclose(f);
+    input_v2(argv[1]);
 
     // multiply a and b
     cpu_t = clock();
@@ -110,7 +217,7 @@ int main(int argc, char *argv[]) {
     cpu_t = clock() - cpu_t;
     
     // Output c to file
-    f = fopen(argv[2], "w");
+    FILE *f = fopen(argv[2], "w");
     assert(f);
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < l; j++)
